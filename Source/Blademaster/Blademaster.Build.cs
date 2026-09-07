4a5c290882af162ce04786bc235b4841a9f778a6
Cha Hoseong <hoseong@chahoseong.com>
2026-09-07 19:36:11 +0900

    GameplayAbilities 모듈을 추가하여 의존성 확장

--- Source/Blademaster/Blademaster.Build.cs
+++ Source/Blademaster/Blademaster.Build.cs
@@ -8,9 +8,20 @@ public class Blademaster : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange([
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"InputCore",
+				"EnhancedInput",
+				"GameplayAbilities",
+				"GameplayTags",
+				"GameplayTasks",
+		]);
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+		PrivateDependencyModuleNames.AddRange([
+			
+		]);
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
